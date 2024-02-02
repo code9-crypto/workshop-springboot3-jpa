2c04883ee5bbb7a3bd8db3d9a8b3b3614e6a7533
William <convidadoc38@gmai.com>
2024-02-02 11:42:23 -0300

    OrderItem, many-to-many association with extra attributes PART 2

---
+++ src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -0,0 +1,14 @@
+package com.educandoweb.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.educandoweb.course.entities.OrderItem;
+
+//Esta notação neste caso é opcional
+//Pois a interface já está herdando do JpaRepository
+//OBS.: esta interface é responsável por fazer acesso ao banco de dados em cima da entidade em questão
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long>{
+
+}
